d279468a0f306e757dafc17fa3c01b95cf04872f
ltran6 <Leland_Tran@Intuit.com>
2017-04-04 17:37:11 -0700

    Added SwitchCellDelegate to FiltersViewController

---
+++ Yelp/SwitchCell.swift
@@ -0,0 +1,40 @@
+//
+//  SwitchCell.swift
+//  Yelp
+//
+//  Created by Tran, Leland on 4/4/17.
+//  Copyright © 2017 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+@objc protocol SwitchCellDelegate {
+    @objc optional func switchCell(switchCell: SwitchCell, didChangeValue value: Bool)
+}
+
+class SwitchCell: UITableViewCell {
+
+    @IBOutlet weak var switchLabel: UILabel!
+    @IBOutlet weak var onSwitch: UISwitch!
+    
+    weak var delegate: SwitchCellDelegate?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+        onSwitch.addTarget(self, action: #selector(switchValueChanged), for: UIControlEvents.valueChanged)
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+    @objc func switchValueChanged() {
+        print("switch value changed")
+        delegate?.switchCell?(switchCell: self, didChangeValue: onSwitch.isOn)
+    }
+
+}
